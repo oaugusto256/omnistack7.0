2842997f9052e8ab68de9adac06ca635a9f2c4ed
tavioalves <tavioalves@gmail.com>
2019-06-11 13:59:11 -0300

    feat: Resize image that are sending to the backend

--- day2/src/controllers/PostController.js
+++ day2/src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
   async index(req, res) {
@@ -10,6 +13,13 @@ module.exports = {
   async store(req, res) {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, 'resized', image));
+
+    fs.unlinkSync(req.file.path);
 
     const post = await Post.create({
       author,
