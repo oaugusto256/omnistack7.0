abba859e4b0840a4117f0cae8dc91a0c6416b36a
tavioalves <tavioalves@gmail.com>
2019-06-11 14:03:04 -0300

    fix: Fix image file extension when it is being saved

--- day2/src/controllers/PostController.js
+++ day2/src/controllers/PostController.js
@@ -14,10 +14,13 @@ module.exports = {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
 
+    const [name, ext] = image.split('.');
+    const fileName = `${name}.jpg`;
+
     await sharp(req.file.path)
       .resize(500)
       .jpeg({ quality: 70 })
-      .toFile(path.resolve(req.file.destination, 'resized', image));
+      .toFile(path.resolve(req.file.destination, 'resized', fileName));
 
     fs.unlinkSync(req.file.path);
 
@@ -26,7 +29,7 @@ module.exports = {
       place,
       description,
       hashtags,
-      image
+      image: fileName
     });
 
     return res.json(post);
--- day2/src/index.js
+++ day2/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,8 @@ mongoose.connect(
     useNewUrlParser: true
   }
 );
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
