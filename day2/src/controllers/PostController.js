3607e7b999ca3b6bfe36afb739759a4502316e9f
tavioalves <tavioalves@gmail.com>
2019-06-11 13:50:23 -0300

    feat: Add feature to create likes to a specific post

--- day2/src/controllers/PostController.js
+++ day2/src/controllers/PostController.js
@@ -1,7 +1,11 @@
 const Post = require('../models/Post');
 
 module.exports = {
-  async index(req, res) {},
+  async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
+
+    return res.json(posts);
+  },
 
   async store(req, res) {
     const { author, place, description, hashtags } = req.body;
