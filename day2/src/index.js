8ffcd578797074679e8df23d370aafb7e34a6e0f
tavioalves <tavioalves@gmail.com>
2019-06-11 14:15:28 -0300

    feat and chore: Add socket.io lib, create method to handle work with http and web sockets at the same time

--- day2/src/index.js
+++ day2/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
   'mongodb+srv://admin:admin@cluster0-rptrx.mongodb.net/test?retryWrites=true&w=majority',
@@ -11,8 +15,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
